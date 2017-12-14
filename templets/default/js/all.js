0597588de568f725c2f836395a04f04f121d64df
Wish_Time <wish-time@outlook.com>
2017-12-14 13:57:38 +0800

    Initial commit

---
+++ templets/default/js/all.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('#slides').slides({
+		preload: true,
+		preloadImage: 'http://www.zjmycz.com/templets/mycz/images/loading.gif',
+		play: 5000,
+		pause: 2500,
+		hoverPause: true
+	});
+});
